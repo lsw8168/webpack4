e7ab38553c14f8620544693f56c346c25f2ffea2
lsw8168 <lsw8168@naver.com>
2018-08-30 20:47:34 +0900

    Tree Shaking

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import {cube} from './math.js';
 
 function component() {
-  let element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  return element;
+    let element = document.createElement('pre');
+
+    element.innerHTML = [
+        'Hello webpack!', '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
+
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'none',
+    mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
     plugins: [
         new CleanWebpackPlugin(['dist/*']),
-        new HtmlWebpackPlugin({title: 'Output Management'})
+        new HtmlWebpackPlugin({title: 'Output Management'}),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },

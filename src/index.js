c8cab30e09df246c6f822674421f9b17cbd2a845
lsw8168 <lsw8168@naver.com>
2018-08-30 21:09:55 +0900

    Production

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import _ from 'lodash';
 import './style.css';
 import {cube} from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 function component() {
     let element = document.createElement('pre');
 
     element.innerHTML = [
         'Hello webpack!', '5 cubed is equal to ' + cube(5)
     ].join('\n\n');
+
+    element.classList.add('hello');
 
     return element;
 }
